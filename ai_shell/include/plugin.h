bbb1269cab7e84bb15fd0356fe2ab1d7961d50e1
Bob Tabor <bobtabor@sbcglobal.net>
2026-06-25 14:52:51 -0500

    new stuffs

--- ai_shell/include/plugin.h
+++ ai_shell/include/plugin.h
@@ -9,6 +9,8 @@ plugin_fn plugin_lookup(const char* name);
 
 // Declare plugin functions
 char* plugin_ddg(int argc, char** argv);
+char* plugin_brave(int argc, char** argv);
+char* plugin_chromium(int argc, char** argv);
 char* plugin_summarize_file(int argc, char** argv);
 char* plugin_websearch(int argc, char** argv);
 char* plugin_summarize_file_html(int argc, char** argv);
