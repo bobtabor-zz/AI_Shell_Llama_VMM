23565df0d35821e2f731a76adc4e9f6111ffd933
Bob Tabor <bobtabor@sbcglobal.net>
2026-06-29 01:57:54 -0500

    new web stuffs

--- ai_shell/include/plugin.h
+++ ai_shell/include/plugin.h
@@ -1,7 +1,24 @@
 #ifndef PLUGIN_H
 #define PLUGIN_H
+#ifndef WINVER
+#define WINVER 0x0602 /* Target Windows 8+ */
+#endif
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0602 /* Target Windows 8+ */
+#endif
+/* Manually define the macro if winhttp.h omitted it */
+#ifndef WINHTTP_OPTION_UPGRADE_TO_WEBSOCKET
+#define WINHTTP_OPTION_UPGRADE_TO_WEBSOCKET 114
+#endif
+#include <windows.h>
+#include <winhttp.h>
+#pragma comment(lib, "winhttp.lib")
 
 typedef char* (*plugin_fn)(int argc, char** argv);
+
+HINTERNET chromium_connect(void);
+BOOL ws_send(HINTERNET ws, const char* msg);
+char* ws_recv(HINTERNET ws);
 
 void plugin_register(const char* name, plugin_fn fn);
 plugin_fn plugin_lookup(const char* name);
